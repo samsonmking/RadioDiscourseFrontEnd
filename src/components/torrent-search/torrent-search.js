0a12bc89f572fa5617bcbd87e1e61a338a94f92e
midnightspecial-io <samson@samsonmking.com>
2016-07-24 21:30:25 -0400

    Fixed bootstrap formatting.  Added default image if album art doesn't load.

--- src/components/torrent-search/torrent-search.js
+++ src/components/torrent-search/torrent-search.js
@@ -16,7 +16,7 @@ class Torrent {
 		});
 		this.sizeMb = ko.computed(() => {
 			var mb = this.size() / 1000000
-			return parseFloat(mb).toFixed(2);
+			return parseFloat(mb).toFixed(2) + ' mb';
 		});
 	}
 }
@@ -34,34 +34,46 @@ class TorrentGroup {
 class TorrentSearch {
     constructor(params) {
     	this.whatBaseUri = 'http://192.168.56.2:5000/rd/api/whatcd';
+    	this.torrentBaseUri = 'http://192.168.56.2:5000/rd/api/torrent';
     	this.artistSearch = ko.observable();
     	this.artistName = ko.observable();
     	this.artistResults = ko.observableArray();
+
+    	this.downloadTorrent = (torrent) => {
+    	var url = this.torrentBaseUri + '/' + torrent.id;
+	    	$.post(url)
+	    		.done((reply) => {
+	    			console.log(reply);
+	    		})
+	    		.fail((reply) => {
+	    			console.log(reply);
+	    		});
+	    }
+
+    	this.searchArtist = () => {
+	    	var noSpace = this.artistSearch().replace(' ', '+');
+	    	var url = this.whatBaseUri + '?artist_search=' + noSpace;
+	    	$.getJSON(url)
+	    		.done((reply) => {
+	    			this.artistName(reply.artist_search);
+	    			// Map Torrent Groups (Albums)
+	    			var koGroups = $.map(reply.results, (tg) => {
+	    				var koTg = new TorrentGroup(tg);
+	    				// Map Torrents per group
+	    				var koTorrents = $.map(tg.torrent, (tor) => {
+	    					return new Torrent(tor);
+	    				});
+	    				koTg.torrents(koTorrents);
+	    				return koTg;
+	    			});
+	    			this.artistResults(koGroups);
+	    		})
+	    		.fail((reply) => {
+	    			console.log(reply);
+	    		});
+    	}
     }
 
-    searchArtist() {
-    	var noSpace = this.artistSearch().replace(' ', '+');
-    	var url = this.whatBaseUri + '?artist_search=' + noSpace;
-    	$.getJSON(url)
-    		.done((reply) => {
-    			this.artistName(reply.artist_search);
-    			// Map Torrent Groups (Albums)
-    			var koGroups = $.map(reply.results, (tg) => {
-    				var koTg = new TorrentGroup(tg);
-    				// Map Torrents per group
-    				var koTorrents = $.map(tg.torrent, (tor) => {
-    					return new Torrent(tor);
-    				});
-    				koTg.torrents(koTorrents);
-    				return koTg;
-    			});
-    			this.artistResults(koGroups);
-    		})
-    		.fail((reply) => {
-    			console.log(reply);
-    		});
-    }
-    
     dispose() {
         // This runs when the component is torn down. Put here any logic necessary to clean up,
         // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
